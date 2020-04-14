66dac1516bf6baadb28150056b022e6c02915f59
Matías Van Der Beken <matiasvdb@hotmail.com>
2020-04-14 15:17:52 -0300

    Modificacion del teclado, ahora funciona correctamente

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
 
 public class Calculadora {
+	
+
 	public static void main(String[] args){            
 		Integer a;            
 		Integer b;           
 		Integer opcion;            
 		Integer resultado;
+		Scanner teclado;
+		
+		teclado = new Scanner(System.in);
+		
+		
 		
 		do{            
 			System.out.println("Ingrese su operación /n 1 para sumar /n 2 pararestar /n 3 para multiplicar /n 4 para dividir");            
-			opcion = teclado.next();            
+			
+			opcion = teclado.nextInt();            
 			}
 		while(opcion < 1 && opcion > 4)   
 			
 		System.out.println("Ingrese el primer numero");            
-		a = teclado.next();  
+		a = teclado.nextInt();  
 		
 		System.out.println("Ingrese el segundo numero");            
-		a = teclado.next();
+		a = teclado.nextInt();
 		
 		if(opcion.equals(1)){                
 			resultado = a + b;                            

5818b84e8282e43118ab70d0dd3b3120ced2540a
sanchez matias <matias-leandro@outlook.com>
2020-04-14 15:26:49 -0300

    se arreglo variables en calculadora.class

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -18,14 +18,13 @@ public class Calculadora {
 			System.out.println("Ingrese su operación /n 1 para sumar /n 2 pararestar /n 3 para multiplicar /n 4 para dividir");            
 			
 			opcion = teclado.nextInt();            
-			}
-		while(opcion < 1 && opcion > 4)   
+			}while(opcion < 1 && opcion > 4);   
 			
 		System.out.println("Ingrese el primer numero");            
 		a = teclado.nextInt();  
 		
 		System.out.println("Ingrese el segundo numero");            
-		a = teclado.nextInt();
+		b = teclado.nextInt();
 		
 		if(opcion.equals(1)){                
 			resultado = a + b;                            
@@ -33,7 +32,7 @@ public class Calculadora {
 		else if(opcion.equals(2)){                
 			resultado = a - b;            
 			}
-		elseif(opcion.equals(3)){                
+		else if(opcion.equals(3)){                
 			resultado = a * b;            
 			}
 		else{                
